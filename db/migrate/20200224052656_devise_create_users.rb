790c022ae8b2af95cbb0f8c2d0e55faf3789381d
m-y-g-daisuke <disk.myg@gmail.com>
2020-02-24 14:40:29 +0900

    after advises from menter
    rails g devise controllerで作った余計なディレクトリの削除
    migration　ファイルの削除項目の修復のために、一度userモデルを消して再度作成。

--- db/migrate/20200224052656_devise_create_users.rb
+++ db/migrate/20200224052656_devise_create_users.rb
@@ -4,18 +4,16 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name,             null: false, unique: true
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-    end
-    add_index :users, :name, unique: true
+      
 
       ## Recoverable
-      #t.string   :reset_password_token
-      #t.datetime :reset_password_sent_at
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
 
       ## Rememberable
-      #t.datetime :remember_created_at
+      t.datetime :remember_created_at
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
@@ -36,11 +34,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       # t.datetime :locked_at
 
 
-      #t.timestamps null: false
-    #end
+      t.timestamps null: false
+    end
 
     add_index :users, :email,                unique: true
-    #add_index :users, :reset_password_token, unique: true
+    add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
