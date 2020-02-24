2e52e185a7f00cc49b9e649af0272d7d7a6fe54a
m-y-g-daisuke <disk.myg@gmail.com>
2020-02-24 15:06:23 +0900

    remake migration file & db:migrate　=>error resolved!!
    userテーブルのマイグレーションファイルのリセットの後に
    新しいファイルでrails db:migrate

--- db/migrate/20200224052656_devise_create_users.rb
+++ db/migrate/20200224052656_devise_create_users.rb
@@ -4,9 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,               null: false, unique: true
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      
+
 
       ## Recoverable
       t.string   :reset_password_token
@@ -39,7 +40,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+    add_index :users, :name,                 unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
+
   end
 end
