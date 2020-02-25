3bba7297a239f7369911fbc2987a28c375416b37
m-y-g-daisuke <disk.myg@gmail.com>
2020-02-25 13:59:25 +0900

    install gem carrierwave&mini_magick , mout_uploader on message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,18 @@ class MessagesController < ApplicationController
   def index
   end
 
-  def create
+  def create(message_params)
+    @message=Message.new
+    if @message.save
+      redirect_to group_message_index_path(params[:id]), notice: "メッセージが送信されました"
+    else
+      render :index
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:name, :image, :user_id, :group_id)
   end
 
 end
