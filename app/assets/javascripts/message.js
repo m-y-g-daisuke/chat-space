573b9e4bbd973c8f1d7c609fe5311e46b9bd35a7
m-y-g-daisuke <disk.myg@gmail.com>
2020-02-28 16:59:34 +0900

    create html from json

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,39 @@
 $(function(){
+
+  function buildHTML(message){
+    if (message.image) {
+      var html =
+       `<div class="message--info">
+          <div class="message--info__name">
+            ${message.user_name}
+          </div>
+          <div class="message--info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message--posts">
+          <div class="message--text">
+            ${message.content}
+          </div>
+          <img class="message--image" ${message.image} >
+        </div>` //メッセージに画像が含まれる場合のHTMLを作る
+    } else {
+      var html = 
+        `<div class="message--info">
+          <div class="message--info__name">
+            ${message.user_name}
+          </div>
+          <div class="message--info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message--text"> 
+          ${message.content}
+        </div>`//メッセージに画像が含まれない場合のHTMLを作る
+    }
+    return html
+  }
+
   $("#new_message").on("submit", function(e){
     e.preventDefault();
     var url = $(this).attr("action");
@@ -10,6 +45,11 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $(".contents").append(html);
+      $("form")[0].reset();
     })
   })
 })
