4589356d67413a80f05f778da6f01a9609f1c3aa
m-y-g-daisuke <disk.myg@gmail.com>
2020-02-28 14:40:32 +0900

    With ajax, move message#create when event occured

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,15 @@
 $(function(){
   $("#new_message").on("submit", function(e){
     e.preventDefault();
-    console.log(check)
+    var url = $(this).attr("action");
+    var formData = new FormData(this);
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   })
 })
