286f661398fdceb78c0564fb0cc9246057461b67
m-y-g-daisuke <disk.myg@gmail.com>
2020-02-29 10:59:32 +0900

    send inputted data to model as json data

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,15 @@
 $(function(){
   $("#group_name").on("keyup", function(){
-  var input = $(this).val();
-  console.log(input)
+    var input = $(this).val();
+    $.ajax({
+      type: "GET",
+      url: "/users",
+      data: { keyword: input},
+      dataType: "json"
+    })
+    .done(function(users){
+    });
+    fail(function() {
+    })
   })
 })
